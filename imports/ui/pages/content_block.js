8a4394a12208ced90fc13dcd7f1ee5cd2c0a8fa6
Genroa <genroa@hotmail.fr>
2017-02-24 20:17:52 +0100

    clearing logs

--- imports/ui/pages/content_block.js
+++ imports/ui/pages/content_block.js
@@ -7,7 +7,6 @@ import './content_block.html';
 Template.content_block.events({
 	'click .delete_content_block' : function(evt) {
 		var data = Template.instance().data;
-		console.log("call");
 		Meteor.call("delete_content_block", data.page._id, 
 											data.sectionIndex, 
 											data.columnIndex,
